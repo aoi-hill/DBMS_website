992a8076d43390ae220ec956eea819aaf43f85d9
badaman <78209498+badaman@users.noreply.github.com>
2021-02-25 16:57:52 +0530

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -15,10 +15,7 @@ $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'Reply-To: ' . $_POST["email"] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-  //
-  mail( "khaitawng2014@gmail.com", $_POST['subject'], $_POST['message'], $headers );
+  mail( "amanjaipuria086@gmail.com", $_POST['subject'], $_POST['message'], $headers );
  
-  //      ^
-  //  Replace with your email 
 }
 ?>
